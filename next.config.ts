6f5437f824932d3477db59e79cf847fef75a455f
deepak <deepakkumawat2203@gmail.com>
2026-03-03 21:56:09 +0530

    final deploy fix

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/scroll-hero-project",
-  assetPrefix: "/scroll-hero-project/",
 };
 
 export default nextConfig;
